577624d0974230563f459d4e4c856274bdb2f8eb
jonroger3 <jonroger3@gmail.com>
2018-09-26 19:16:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {architects : "Doomsday", bmth : "Mantra", pixies : "Where Is My Mind"};
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"};
 
 function updatePlaylist(obj, name, title){
   
